3539e07396b9a6a1d0ff763e6f606ad75fc7e066
batmhannJ <reyeshannahjoy82@gmail.com>
2024-10-21 02:13:58 +0800

    try

--- Web/TRAV/LATEST_TRAVICATION/my-app/app.js
+++ Web/TRAV/LATEST_TRAVICATION/my-app/app.js
@@ -11,7 +11,7 @@ const path = require('path');
 
 
 const cors = require("cors")
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:42284', 'http://localhost:43264','https://travication.vercel.app', 'https://travication.vercel.app/login'
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:42284', 'http://localhost:43264','https://travication.vercel.app', 'https://travication.vercel.app/home'
 ]; // Add all allowed origins
 require('dotenv').config();
 
