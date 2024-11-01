0590e98415ed27c851787567e9eadcd570485ca8
batmhannJ <reyeshannahjoy82@gmail.com>
2024-11-01 20:05:44 +0800

    reloading page

--- Web/TRAV/LATEST_TRAVICATION/my-app/src/components/ProtectedRoute.js
+++ Web/TRAV/LATEST_TRAVICATION/my-app/src/components/ProtectedRoute.js
@@ -1,16 +1,26 @@
-import React, { useContext } from 'react';
-import {Navigate , Outlet  } from 'react-router-dom';
+import React, { useContext, useEffect } from 'react';
+import { Navigate , Outlet, useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthProvider';
 
 
 const ProtectedRoute = ({ redirectTo = "/home" }) => {
     const { isAuthenticated, loading } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!loading) {
+            if (isAuthenticated) {
+                // Automatically navigate to the protected route if authenticated
+                navigate(redirectTo, { replace: true });
+            }
+        }
+    }, [isAuthenticated, loading, navigate, redirectTo]);
 
     if (loading) {
         return <div>Loading...</div>; // Optional: Add a loading spinner
     }
 
-    return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} />;
+    return isAuthenticated ? <Outlet /> : <Navigate to={redirectTo} replace />;
 };
 
 export default ProtectedRoute;
