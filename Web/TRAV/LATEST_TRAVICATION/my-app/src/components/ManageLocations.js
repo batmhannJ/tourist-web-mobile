8e10775654293bbec9859bf4901cefdc58304be6
batmhannJ <reyeshannahjoy82@gmail.com>
2025-01-22 23:56:31 +0800

    try for date

--- Web/TRAV/LATEST_TRAVICATION/my-app/src/components/ManageLocations.js
+++ Web/TRAV/LATEST_TRAVICATION/my-app/src/components/ManageLocations.js
@@ -372,7 +372,7 @@ useEffect(() => {
                             <td>{location.latitude}</td>
                             <td>{location.longitude}</td>
                             <td>{location.description}</td>
-                            <td>{new Date(location.dateAdded).toLocaleDateString()}</td>
+                            <td>{location.dateAdded}</td>
 
                             <td>
                                 {location.image ? (
